aceefce043b635416d9e77297eb5c662efbd8737
covyvi <davidcantumartinez@gmail.com>
2021-10-17 16:31:39 -0500

    Distance ready

--- 05-knn/knn.py
+++ 05-knn/knn.py
@@ -15,6 +15,8 @@ class Knn:
         # TODO: Implement the euclidean distance function to calculate the distance between the provided elements
         # The output should be a scalar
         distance = 0.0
+        distance = np.sqrt(np.sum((example1-example2)**2))
+        print(distance)
         return distance
 
     def get_neighbors(self, example):
@@ -22,7 +24,12 @@ class Knn:
         # You must return an np array of shape 1 x k, each element must be the index of the neighbour so you can associate it with its class/label later.
         # You will need to calculate the distance and then sort, recalling the indexes. Do your research to find a method that allows this.
         # Remember that in self.model_x you have your list of examples
-        return np.zeros((1, self.k))
+        print(example)
+        distances = [self.euclidean_distance(x, example) for x in self.model_x]
+        k_indices = np.argsort(distances)[:self.k]
+        print(k_indices.shape)
+        k_nerest_labels = [self.model_y[i] for i in k_indices]
+        return k_indices
 
     def fit(self, X, y):
         """
@@ -40,4 +47,6 @@ class Knn:
         # For each example, you will need to get the indexes of nearest neighbours and then the voting.
         # Do your research to find numpy or python functions that allow to sort and count
         # In self.model_y you will have the list of the classes for examples in original dataset (self.model_x). self.model_y is of shape 1xm
+        x = self.get_neighbors(example)
+        #labels = self.model_y[x]
         return 0
